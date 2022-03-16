d2b106963d22ad0f7970b8cd33b39348c1c36e0a
Francisco Laurindo <chiconeto.francisco@hotmail.com>
2022-03-16 18:07:46 -0300

    update exercicio 87

--- exercicios/ex087.py
+++ exercicios/ex087.py
@@ -17,17 +17,26 @@ R$ 1.100,00     100             3                       R$    366,00
 R$ 1.150,00     150             6                       R$    191,67
 """
 
-value = float(input('Valor da dívida: R$ '))
-percent = 10
-parcel = 0
-value_increase = 0
+value = int(input('\nInsira um valor: R$ '))
+count_parcels = 0
 
-for i in range(1, 5):
-    increase = (percent / 100 * value)
-    percent += 5
-    value_increased = increase + value
-    print(value_increased)
+# Formatação do Início
+print('-'*110)
+print('Valor da Dívida ---------- Valor dos Juros ----------- Quantidade de Parcelas ---------- Valor da Parcela')
 
-    for j in range(1, 6):
-        parcel = value_increased / i
-        print(parcel)
+# Usei um while aninhado para calcular o valor da parcela dentro do valor da dívida com juros
+for i in range(10, 26, 5):
+    value_increased = ((i / 100) * value) + value
+    fees = value_increased - value
+    print(f'R$ {value_increased}', end=' ')
+    print(f'{fees:>27}', end=' ')
+
+    while 0 <= 12:
+        count_parcels += 3
+        value_parcels = value_increased / count_parcels
+        break
+
+    print(f'{count_parcels:>27}', end=' ')
+    print(f'{value_parcels:>32.2f}')
+
+print('-'*110)
